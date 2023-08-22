d9fc9873db3de91a4ba983d87bf7677ca118d318
MarioGuTe <mariogutierreztello@gmail.com>
2023-08-21 23:59:58 -0400

    add atoms folder

---
+++ src/atoms/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+import Logo from "../molecules/Logo/Logo";
+import SearchBar from "../molecules/SearchBar/SearchBar";
+import TvShowDetail from "../molecules/TvShowDetail/TvShowDetail";
+import TvShowList from "../molecules/TvShowList/TvShowList";
+import s from "./particle/style.module.css";
+
+function App() {
+  return (
+    <>
+      <h1 style={{ color: "black" }}>Hello im app!</h1>
+    </>
+  );
+}
+
+export default App;
