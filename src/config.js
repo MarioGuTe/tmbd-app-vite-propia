b02bbe4b3e7437e8241ad8950cc6d811dda452c6
MarioGuTe <mariogutierreztello@gmail.com>
2023-08-23 00:59:33 -0400

    fix base url

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
-const BASE_URL = "https://api.themoviedb.org/3";
+const BASE_URL = "https://api.themoviedb.org/3/";
 const API_KEY_PARAM = "?api_key=e9e3fa63abacea322e4f9888a630a995";
 const BACKDROP_BASE_URL = "https://image.tmdb.org/t/p/original";
 const SMALL_IMAGE_COVER_BASE_URL = "https://image.tmdb.org/t/p/w300";
