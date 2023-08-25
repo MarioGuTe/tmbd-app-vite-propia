2985dd9d38081c6c046ec1f652d5f8ba2e4fd40f
MarioGuTe <mariogutierreztello@gmail.com>
2023-08-25 16:45:39 -0400

    add logic and style

--- src/molecules/TvShowDetail/TvShowDetail.jsx
+++ src/molecules/TvShowDetail/TvShowDetail.jsx
@@ -1,7 +1,26 @@
-import React from "react";
+import PropTypes from "prop-types";
+import FiveStarRating from "../FiveStarRating/FiveStarRating";
+import s from "./particle/style.module.css";
 
-const TvShowDetail = () => {
-  return <div style={{ color: "black" }}>I am tvshoedetail</div>;
+const TvShowDetail = ({ tvShow }) => {
+  const rating = tvShow.vote_average / 2;
+
+  return (
+    <>
+      <div className={s.title}>{tvShow.name}</div>
+      <div className={s.rating_container}>
+        <FiveStarRating rating={rating} />
+        <span className={s.rating}>{rating}/5</span>
+      </div>
+      <div className={s.overview}>
+        {tvShow.overview.length > 1 ? tvShow.overview : "No description found"}
+      </div>
+    </>
+  );
+};
+
+TvShowDetail.propTypes = {
+  tvShow: PropTypes.object,
 };
 
 export default TvShowDetail;
