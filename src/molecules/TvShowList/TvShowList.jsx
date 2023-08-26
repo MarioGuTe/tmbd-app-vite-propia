e338690564288447aad7c97c94f6691d0e1d9ae0
MarioGuTe <mariogutierreztello@gmail.com>
2023-08-26 00:03:24 -0400

    add logic and style

--- src/molecules/TvShowList/TvShowList.jsx
+++ src/molecules/TvShowList/TvShowList.jsx
@@ -1,7 +1,25 @@
-import React from "react";
+import PropTypes from "prop-types";
+import TvShowListItem from "../TvShowListItem/TvShowListItem";
+import s from "./particle/style.module.css";
 
-const TvShowList = () => {
-  return <div style={{ color: "black" }}>I am tvshowlist</div>;
+const TvShowList = ({ onClickItem, tvShowList }) => (
+  <>
+    <div className={s.title}>You probably like:</div>
+    <div className={s.list}>
+      {tvShowList.map((tvShow) => {
+        return (
+          <span key={tvShow.id}>
+            <TvShowListItem tvShow={tvShow} onClick={onClickItem} />
+          </span>
+        );
+      })}
+    </div>
+  </>
+);
+
+TvShowList.propTypes = {
+  onClickItem: PropTypes.func,
+  tvShowList: PropTypes.array,
 };
 
 export default TvShowList;
