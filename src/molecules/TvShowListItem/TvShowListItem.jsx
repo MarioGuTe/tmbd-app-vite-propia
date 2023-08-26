7b136e325e3bd602a4917f2bfbe6bc434bf55781
MarioGuTe <mariogutierreztello@gmail.com>
2023-08-26 18:15:54 -0400

    add logic and style

--- src/molecules/TvShowListItem/TvShowListItem.jsx
+++ src/molecules/TvShowListItem/TvShowListItem.jsx
@@ -1,7 +1,31 @@
-import React from "react";
+import PropTypes from "prop-types";
+import { SMALL_IMAGE_COVER_BASE_URL, MAX_TITLE_CHAR } from "../../config";
+import s from "./particle/style.module.css";
 
-const TvShowListItem = () => {
-  return <div>I am tvshowlistitem</div>;
+const TvShowListItem = ({ tvShow, onClick }) => {
+  const onClickHandle = () => {
+    onClick(tvShow);
+  };
+
+  return (
+    <div onClick={onClickHandle} className={s.container}>
+      <img
+        alt={tvShow.name}
+        src={SMALL_IMAGE_COVER_BASE_URL + tvShow.backdrop_path}
+        className={s.img}
+      />
+      <div className={s.title}>
+        {tvShow.name.length > MAX_TITLE_CHAR
+          ? tvShow.name.slice(0, MAX_TITLE_CHAR) + "..."
+          : tvShow.name}
+      </div>
+    </div>
+  );
+};
+
+TvShowListItem.propTypes = {
+  tvShow: PropTypes.object,
+  onClick: PropTypes.func,
 };
 
 export default TvShowListItem;
